3711d7f5cc865aa1e58cac5491b10ee205caac99
TheInfinityGod <kumarammy7@gmail.com>
2024-03-02 18:57:04 +0530

    added

--- src/main/java/com/application/springboot2/TestControllers/myManagementhandler.java
+++ src/main/java/com/application/springboot2/TestControllers/myManagementhandler.java
@@ -3,7 +3,7 @@ package com.application.springboot2.TestControllers;
 public class myManagementhandler {
 
     public static RetrieveFormsResponse myMethod(RetrieveFormsRequest retrieveFormsRequest, String ind) {
-        // TODO Auto-generated method stub
+        //m// TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'myMethod'");
     }
 
